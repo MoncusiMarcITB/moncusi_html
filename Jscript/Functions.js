721e5767b670b00d009db3539cf218fbaed729e4
MoncusiMarcITB <93705188+MoncusiMarcITB@users.noreply.github.com>
2021-11-04 09:51:37 +0100

    Add files via upload

---
+++ Jscript/Functions.js
@@ -0,0 +1,6 @@
+function Color(){
+        document.getElementById("cuadrado").style.backgroundColor="green";
+}
+function ColorBack(){
+    document.getElementById("cuadrado").style.backgroundColor="crimson";
+}
