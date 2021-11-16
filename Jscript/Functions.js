80df674349e1896889ce222cc7150d8fbde32bb5
MoncusiMarcITB <93705188+MoncusiMarcITB@users.noreply.github.com>
2021-11-16 09:33:43 +0100

    Delete Jscript directory

--- Jscript/Functions.js
+++
@@ -1,6 +0,0 @@
-function Color(){
-        document.getElementById("cuadrado").style.backgroundColor="green";
-}
-function ColorBack(){
-    document.getElementById("cuadrado").style.backgroundColor="crimson";
-}
